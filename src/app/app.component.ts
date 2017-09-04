f8a91c74fed0b72d2060ae98de5bf5343e3a3ded
ShahinShabnam <shabnam.shahin@rancelab.com>
2017-09-04 10:49:55 +0530

    value is showing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-root',
-    templateUrl: 'app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-export class AppComponent { }
+export class AppComponent {
+  title = 'app';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,34 @@
-﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
-import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
 
-import { AlertComponent } from './_directives/index';
-import { AlertService } from './_services/index';
-import { HomeComponent } from './home/index';
+import { MdButtonModule } from '@angular/material';
+import { MdDialogModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
+
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        routing
-    ],
-    declarations: [
-        AppComponent,
-        AlertComponent,
-        HomeComponent
-    ],
-    providers: [
-        AlertService
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DialogDemoComponent,
+    MyDialogComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdDialogModule,
+   
+  ],
+  entryComponents: [
+    MyDialogComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
 export class AppModule { }
