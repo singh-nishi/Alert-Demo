e20822215f878485cdded120f1aa6852f5567304
ShahinShabnam <shabnam.shahin@rancelab.com>
2017-09-06 16:23:36 +0530

    done with prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component,OnInit,ViewChild,ViewEncapsulation } from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {Message} from 'primeng/primeng';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
-
-// import { dialogBoxService} from './dialogBox';
 import {ConfirmationService} from "primeng/components/common/api";
 @Component({
    moduleId: module.id,
@@ -14,40 +12,20 @@ import {ConfirmationService} from "primeng/components/common/api";
 })
 export class AppComponent  {
 
-//  private title : string = 'dialogBox';
-//   private displayDialog: boolean;
-//   private header : string;
-//   private messagebody : string;
    name: string;
     userResponse: Message[]=[];
     theUserSaid: string;
-    // dirty: boolean;
-    // username: string;
+    
   msgs: Message[] = [];
     constructor(private confirmationService: ConfirmationService) {
-            // this.header = "Header Content from Component";
-            // this.messagebody = "Message body from App Component";
+        
     }
-
-    
-//   private showDialog() : void {  
-//     this.dialogBoxservice.showDialog(this.header ,this.messagebody);
-//   }
-   
-  showInfo() {
+showInfo() {
     this.msgs = [];
     this.msgs.push({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
-  }
-//   onSubmit(f: FormGroup) {
-//         this.dirty = f.dirty;
-//         this.router.navigate(['/second-view']);
-//     }
-//  onChangeEvent({target}){
-//         this.name = target.value;
-//         console.log(this.name);
-//     }
+    }
 
-    greetMe(){
+greetMe(){
 
         this.confirmationService.confirm({
             message: ` Hey ${this.name}, do you like PrimeNG?`,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {GrowlModule} from 'primeng/primeng';
 import { InputTextModule, ButtonModule, DataTableModule, DialogModule}  from 'primeng/primeng';
-// import { dialogBoxService,dialogComponent} from './dialogBox';
 import {ConfirmDialogModule} from 'primeng/components/confirmdialog/confirmdialog';
 import {ConfirmationService} from 'primeng/components/common/api';
 
